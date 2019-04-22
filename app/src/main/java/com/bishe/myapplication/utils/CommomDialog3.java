759e7a4de8ec7667a07584b65540357cddd9ed25
lenovo <420401567@qq.com>
2019-04-22 22:16:25 +0800

    tw:修改日历记录未完成

--- app/src/main/java/com/bishe/myapplication/utils/CommomDialog3.java
+++ app/src/main/java/com/bishe/myapplication/utils/CommomDialog3.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -52,35 +53,35 @@ public class CommomDialog3 extends Dialog implements View.OnClickListener {
     /**
      * ddd
      */
-    private TextView mTvDwon1;
+    private Button mTvDwon1;
     /**
      * ddd
      */
-    private TextView mTvDwon2;
+    private Button mTvDwon2;
     /**
      * ddd
      */
-    private TextView mTvDwon3;
+    private Button mTvDwon3;
     /**
      * ddd
      */
-    private TextView mTvDwon4;
+    private Button mTvDwon4;
     /**
      * ddd
      */
-    private TextView mTvDwon5;
+    private Button mTvDwon5;
     /**
      * ddd
      */
-    private TextView mTvDwon6;
+    private Button mTvDwon6;
     /**
      * ddd
      */
-    private TextView mTvDwon7;
+    private Button mTvDwon7;
     /**
      * ddd
      */
-    private TextView mTvDwon8;
+    private Button mTvDwon8;
     private LinearLayout mLayDwon;
     /**
      * 经期
@@ -190,21 +191,21 @@ public class CommomDialog3 extends Dialog implements View.OnClickListener {
         mTvTop7.setOnClickListener(this);
         mTvTop8 = (TextView) findViewById(R.id.tv_top8);
         mTvTop8.setOnClickListener(this);
-        mTvDwon1 = (TextView) findViewById(R.id.tv_dwon1);
+        mTvDwon1 = (Button) findViewById(R.id.tv_dwon1);
         mTvDwon1.setOnClickListener(this);
-        mTvDwon2 = (TextView) findViewById(R.id.tv_dwon2);
+        mTvDwon2 = (Button) findViewById(R.id.tv_dwon2);
         mTvDwon2.setOnClickListener(this);
-        mTvDwon3 = (TextView) findViewById(R.id.tv_dwon3);
+        mTvDwon3 = (Button) findViewById(R.id.tv_dwon3);
         mTvDwon3.setOnClickListener(this);
-        mTvDwon4 = (TextView) findViewById(R.id.tv_dwon4);
+        mTvDwon4 = (Button) findViewById(R.id.tv_dwon4);
         mTvDwon4.setOnClickListener(this);
-        mTvDwon5 = (TextView) findViewById(R.id.tv_dwon5);
+        mTvDwon5 = (Button) findViewById(R.id.tv_dwon5);
         mTvDwon5.setOnClickListener(this);
-        mTvDwon6 = (TextView) findViewById(R.id.tv_dwon6);
+        mTvDwon6 = (Button) findViewById(R.id.tv_dwon6);
         mTvDwon6.setOnClickListener(this);
-        mTvDwon7 = (TextView) findViewById(R.id.tv_dwon7);
+        mTvDwon7 = (Button) findViewById(R.id.tv_dwon7);
         mTvDwon7.setOnClickListener(this);
-        mTvDwon8 = (TextView) findViewById(R.id.tv_dwon8);
+        mTvDwon8 = (Button) findViewById(R.id.tv_dwon8);
         mTvDwon8.setOnClickListener(this);
         mLayDwon = (LinearLayout) findViewById(R.id.lay_dwon);
         mLayDwon.setOnClickListener(this);
@@ -217,6 +218,15 @@ public class CommomDialog3 extends Dialog implements View.OnClickListener {
         mTvTitleXinqing.setOnClickListener(this);
         mTvTitleZhengzhuang = (TextView) findViewById(R.id.tv_title_zhengzhuang);
         mTvTitleZhengzhuang.setOnClickListener(this);
+
+        mTvDwon1.setText("量多");
+        mTvDwon2.setText("中等");
+        mTvDwon3.setText("较少");
+        mTvDwon4.setText("很少");
+        mTvDwon5.setText("无感");
+        mTvDwon6.setText("微痛");
+        mTvDwon7.setText("很痛");
+        mTvDwon8.setText("很少");
     }
 
 
@@ -238,67 +248,194 @@ public class CommomDialog3 extends Dialog implements View.OnClickListener {
             default:
                 break;
             case R.id.tv_top1:
-                mTvDwon1.setText(mTvTop1.getText().toString());
+                mTvDwon1.setEnabled(true);
+                mTvDwon2.setEnabled(true);
+                mTvDwon3.setEnabled(true);
+                mTvDwon4.setEnabled(true);
+
+                switch (select) {
+                    case 1:
+                        mTvDwon1.setVisibility(View.VISIBLE);
+                        mTvTop1.setVisibility(View.GONE);
+                        mTvDwon1.setText(mTvTop1.getText().toString());
+                        break;
+                    case 2:
+                        mTvDwon2.setVisibility(View.VISIBLE);
+                        mTvTop1.setVisibility(View.GONE);
+                        mTvDwon2.setText(mTvTop1.getText().toString());
+                        break;
+                    case 3:
+                        mTvDwon3.setVisibility(View.VISIBLE);
+                        mTvTop1.setVisibility(View.GONE);
+                        mTvDwon3.setText(mTvTop1.getText().toString());
+                        break;
+                    case 4:
+                        mTvDwon4.setVisibility(View.VISIBLE);
+                        mTvTop1.setVisibility(View.GONE);
+                        mTvDwon4.setText(mTvTop1.getText().toString());
+                        break;
+                }
+
                 break;
             case R.id.tv_top2:
+                mTvDwon5.setEnabled(true);
+                mTvDwon6.setEnabled(true);
+                mTvDwon7.setEnabled(true);
+                mTvDwon8.setEnabled(true);
+                switch (select2) {
+                    case 5:
+                        mTvDwon5.setVisibility(View.VISIBLE);
+                        mTvTop2.setVisibility(View.GONE);
+                        mTvDwon5.setText(mTvTop2.getText().toString());
+                        break;
+                    case 6:
+                        mTvDwon6.setVisibility(View.VISIBLE);
+                        mTvTop2.setVisibility(View.GONE);
+                        mTvDwon6.setText(mTvTop2.getText().toString());
+                        break;
+                    case 7:
+                        mTvDwon7.setVisibility(View.VISIBLE);
+                        mTvTop2.setVisibility(View.GONE);
+                        mTvDwon7.setText(mTvTop2.getText().toString());
+                        break;
+                    case 8:
+                        mTvDwon8.setVisibility(View.VISIBLE);
+                        mTvTop2.setVisibility(View.GONE);
+                        mTvDwon8.setText(mTvTop2.getText().toString());
+                        break;
+                }
                 break;
             case R.id.tv_top3:
+                mTvDwon3.setVisibility(View.VISIBLE);
+                mTvTop3.setVisibility(View.GONE);
+                mTvDwon3.setText(mTvTop3.getText().toString());
                 break;
             case R.id.tv_top4:
+                mTvDwon4.setVisibility(View.VISIBLE);
+                mTvTop4.setVisibility(View.GONE);
+                mTvDwon4.setText(mTvTop4.getText().toString());
                 break;
             case R.id.tv_top5:
+                mTvDwon5.setVisibility(View.VISIBLE);
+                mTvTop5.setVisibility(View.GONE);
+                mTvDwon5.setText(mTvTop5.getText().toString());
                 break;
             case R.id.tv_top6:
+                mTvDwon6.setVisibility(View.VISIBLE);
+                mTvTop6.setVisibility(View.GONE);
+                mTvDwon4.setText(mTvTop6.getText().toString());
                 break;
             case R.id.tv_top7:
+                mTvDwon7.setVisibility(View.VISIBLE);
+                mTvTop7.setVisibility(View.GONE);
+                mTvDwon7.setText(mTvTop7.getText().toString());
                 break;
             case R.id.tv_top8:
+                mTvDwon8.setVisibility(View.VISIBLE);
+                mTvTop8.setVisibility(View.GONE);
+                mTvDwon8.setText(mTvTop8.getText().toString());
                 break;
             case R.id.tv_dwon1:
+                select = 1;
+                mTvDwon2.setEnabled(false);
+                mTvDwon3.setEnabled(false);
+                mTvDwon4.setEnabled(false);
                 mTvTop1.setText(mTvDwon1.getText().toString());
                 mTvDwon1.setVisibility(View.INVISIBLE);
                 mTvTop1.setVisibility(View.VISIBLE);
                 break;
             case R.id.tv_dwon2:
-                mTvTop2.setText(mTvDwon2.getText().toString());
+                mTvDwon1.setEnabled(false);
+                mTvDwon3.setEnabled(false);
+                mTvDwon4.setEnabled(false);
+                select = 2;
+                mTvTop1.setText(mTvDwon2.getText().toString());
                 mTvDwon2.setVisibility(View.INVISIBLE);
-                mTvTop2.setVisibility(View.VISIBLE);
+                mTvTop1.setVisibility(View.VISIBLE);
                 break;
             case R.id.tv_dwon3:
-                mTvTop3.setText(mTvDwon3.getText().toString());
+                select = 3;
+                mTvDwon1.setEnabled(false);
+                mTvDwon2.setEnabled(false);
+                mTvDwon4.setEnabled(false);
+                mTvTop1.setText(mTvDwon3.getText().toString());
                 mTvDwon3.setVisibility(View.INVISIBLE);
-                mTvTop3.setVisibility(View.VISIBLE);
+                mTvTop1.setVisibility(View.VISIBLE);
                 break;
             case R.id.tv_dwon4:
-                mTvTop4.setText(mTvDwon4.getText().toString());
+                select = 4;
+                mTvDwon1.setEnabled(false);
+                mTvDwon3.setEnabled(false);
+                mTvDwon2.setEnabled(false);
+                mTvTop1.setText(mTvDwon4.getText().toString());
                 mTvDwon4.setVisibility(View.INVISIBLE);
-                mTvTop4.setVisibility(View.VISIBLE);
+                mTvTop1.setVisibility(View.VISIBLE);
                 break;
             case R.id.tv_dwon5:
-                mTvTop5.setText(mTvDwon5.getText().toString());
+                select2 = 5;
+                mTvDwon6.setEnabled(false);
+                mTvDwon7.setEnabled(false);
+                mTvDwon8.setEnabled(false);
+                mTvTop2.setText(mTvDwon5.getText().toString());
                 mTvDwon5.setVisibility(View.INVISIBLE);
-                mTvTop5.setVisibility(View.VISIBLE);
+                mTvTop2.setVisibility(View.VISIBLE);
                 break;
             case R.id.tv_dwon6:
-                mTvTop6.setText(mTvDwon6.getText().toString());
+                select2 = 6;
+                mTvDwon5.setEnabled(false);
+                mTvDwon7.setEnabled(false);
+                mTvDwon8.setEnabled(false);
+                mTvTop2.setText(mTvDwon6.getText().toString());
                 mTvDwon6.setVisibility(View.INVISIBLE);
-                mTvTop6.setVisibility(View.VISIBLE);
+                mTvTop2.setVisibility(View.VISIBLE);
                 break;
             case R.id.tv_dwon7:
-                mTvTop7.setText(mTvDwon7.getText().toString());
+                select2 = 7;
+                mTvDwon5.setEnabled(false);
+                mTvDwon6.setEnabled(false);
+                mTvDwon8.setEnabled(false);
+                mTvTop2.setText(mTvDwon7.getText().toString());
                 mTvDwon7.setVisibility(View.INVISIBLE);
-                mTvTop7.setVisibility(View.VISIBLE);
+                mTvTop2.setVisibility(View.VISIBLE);
                 break;
             case R.id.tv_dwon8:
-                mTvTop8.setText(mTvDwon8.getText().toString());
+                select2 = 8;
+                mTvDwon5.setEnabled(false);
+                mTvDwon7.setEnabled(false);
+                mTvDwon6.setEnabled(false);
+                mTvTop2.setText(mTvDwon8.getText().toString());
                 mTvDwon8.setVisibility(View.INVISIBLE);
-                mTvTop8.setVisibility(View.VISIBLE);
+                mTvTop2.setVisibility(View.VISIBLE);
                 break;
             case R.id.lay_dwon:
                 break;
 
             case R.id.tv_title_jingqi:
+                mTvDwon1.setEnabled(true);
+                mTvDwon2.setEnabled(true);
+                mTvDwon3.setEnabled(true);
+                mTvDwon4.setEnabled(true);
+                mTvDwon5.setEnabled(true);
+                mTvDwon6.setEnabled(true);
+                mTvDwon7.setEnabled(true);
+                mTvDwon8.setEnabled(true);
                 mLayDwon.setVisibility(View.VISIBLE);
+                mTvDwon1.setVisibility(View.VISIBLE);
+                mTvDwon2.setVisibility(View.VISIBLE);
+                mTvDwon3.setVisibility(View.VISIBLE);
+                mTvDwon4.setVisibility(View.VISIBLE);
+                mTvDwon5.setVisibility(View.VISIBLE);
+                mTvDwon6.setVisibility(View.VISIBLE);
+                mTvDwon7.setVisibility(View.VISIBLE);
+                mTvDwon8.setVisibility(View.VISIBLE);
+                mTvTop1.setVisibility(View.GONE);
+                mTvTop2.setVisibility(View.GONE);
+                mTvTop3.setVisibility(View.INVISIBLE);
+                mTvTop4.setVisibility(View.INVISIBLE);
+                mTvTop5.setVisibility(View.INVISIBLE);
+                mTvTop6.setVisibility(View.INVISIBLE);
+                mTvTop7.setVisibility(View.INVISIBLE);
+                mTvTop8.setVisibility(View.INVISIBLE);
                 mTvDwon1.setText("量多");
                 mTvDwon2.setText("中等");
                 mTvDwon3.setText("较少");
@@ -309,15 +446,51 @@ public class CommomDialog3 extends Dialog implements View.OnClickListener {
                 mTvDwon8.setText("很少");
                 break;
             case R.id.tv_title_aiai:
+                mTvDwon1.setEnabled(true);
+                mTvDwon2.setEnabled(true);
+                mTvDwon3.setEnabled(true);
+                mTvDwon4.setEnabled(true);
+                mTvDwon5.setEnabled(true);
+                mTvDwon6.setEnabled(true);
+                mTvDwon7.setEnabled(true);
+                mTvDwon8.setEnabled(true);
                 mLayDwon.setVisibility(View.GONE);
                 mTvDwon1.setText("无措施");
                 mTvDwon2.setText("避孕套");
                 mTvDwon3.setText("避孕药");
                 mTvDwon4.setText("体外排精");
-
+                mTvDwon1.setVisibility(View.VISIBLE);
+                mTvDwon2.setVisibility(View.VISIBLE);
+                mTvDwon3.setVisibility(View.VISIBLE);
+                mTvDwon4.setVisibility(View.VISIBLE);
+                mTvTop1.setVisibility(View.GONE);
+                mTvTop2.setVisibility(View.GONE);
+                mTvTop3.setVisibility(View.INVISIBLE);
+                mTvTop4.setVisibility(View.INVISIBLE);
+                mTvTop5.setVisibility(View.INVISIBLE);
+                mTvTop6.setVisibility(View.INVISIBLE);
+                mTvTop7.setVisibility(View.INVISIBLE);
+                mTvTop8.setVisibility(View.INVISIBLE);
                 break;
             case R.id.tv_title_xinqing:
+                mTvDwon1.setEnabled(true);
+                mTvDwon2.setEnabled(true);
+                mTvDwon3.setEnabled(true);
+                mTvDwon4.setEnabled(true);
+                mTvDwon5.setEnabled(true);
+                mTvDwon6.setEnabled(true);
+                mTvDwon7.setEnabled(true);
+                mTvDwon8.setEnabled(true);
                 mLayDwon.setVisibility(View.VISIBLE);
+                mLayDwon.setVisibility(View.VISIBLE);
+                mTvDwon1.setVisibility(View.VISIBLE);
+                mTvDwon2.setVisibility(View.VISIBLE);
+                mTvDwon3.setVisibility(View.VISIBLE);
+                mTvDwon4.setVisibility(View.VISIBLE);
+                mTvDwon5.setVisibility(View.VISIBLE);
+                mTvDwon6.setVisibility(View.VISIBLE);
+                mTvDwon7.setVisibility(View.VISIBLE);
+                mTvDwon8.setVisibility(View.VISIBLE);
                 mTvDwon1.setText("开心");
                 mTvDwon2.setText("生气");
                 mTvDwon3.setText("难过");
@@ -326,9 +499,33 @@ public class CommomDialog3 extends Dialog implements View.OnClickListener {
                 mTvDwon6.setText("平静");
                 mTvDwon7.setText("活泼");
                 mTvDwon8.setText("紧张");
+                mTvTop1.setVisibility(View.INVISIBLE);
+                mTvTop2.setVisibility(View.INVISIBLE);
+                mTvTop3.setVisibility(View.INVISIBLE);
+                mTvTop4.setVisibility(View.INVISIBLE);
+                mTvTop5.setVisibility(View.INVISIBLE);
+                mTvTop6.setVisibility(View.INVISIBLE);
+                mTvTop7.setVisibility(View.INVISIBLE);
+                mTvTop8.setVisibility(View.INVISIBLE);
                 break;
             case R.id.tv_title_zhengzhuang:
+                mTvDwon1.setEnabled(true);
+                mTvDwon2.setEnabled(true);
+                mTvDwon3.setEnabled(true);
+                mTvDwon4.setEnabled(true);
+                mTvDwon5.setEnabled(true);
+                mTvDwon6.setEnabled(true);
+                mTvDwon7.setEnabled(true);
+                mTvDwon8.setEnabled(true);
                 mLayDwon.setVisibility(View.VISIBLE);
+                mTvDwon1.setVisibility(View.VISIBLE);
+                mTvDwon2.setVisibility(View.VISIBLE);
+                mTvDwon3.setVisibility(View.VISIBLE);
+                mTvDwon4.setVisibility(View.VISIBLE);
+                mTvDwon5.setVisibility(View.VISIBLE);
+                mTvDwon6.setVisibility(View.VISIBLE);
+                mTvDwon7.setVisibility(View.VISIBLE);
+                mTvDwon8.setVisibility(View.VISIBLE);
                 mTvDwon1.setText("腰酸");
                 mTvDwon2.setText("头疼");
                 mTvDwon3.setText("长痘");
@@ -337,9 +534,20 @@ public class CommomDialog3 extends Dialog implements View.OnClickListener {
                 mTvDwon6.setText("失眠");
                 mTvDwon7.setText("恶心");
                 mTvDwon8.setText("眩晕");
+                mTvTop1.setVisibility(View.INVISIBLE);
+                mTvTop2.setVisibility(View.INVISIBLE);
+                mTvTop3.setVisibility(View.INVISIBLE);
+                mTvTop4.setVisibility(View.INVISIBLE);
+                mTvTop5.setVisibility(View.INVISIBLE);
+                mTvTop6.setVisibility(View.INVISIBLE);
+                mTvTop7.setVisibility(View.INVISIBLE);
+                mTvTop8.setVisibility(View.INVISIBLE);
                 break;
         }
     }
+
+    private int select = 0;
+    private int select2 = 0;
 
     public interface OnCloseListener {
         void onClick(Dialog dialog, boolean confirm);
